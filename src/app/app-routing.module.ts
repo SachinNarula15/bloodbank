56eed966c5abf7e785d56b1bf1e3d7f71151762b
SachinNarula15 <36657726+SachinNarula15@users.noreply.github.com>
2018-09-24 16:00:24 +0530

    Home Image Created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RepositoryComponent } from './repository/repository.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'repo', component: RepositoryComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
